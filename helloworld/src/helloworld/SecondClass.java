c93d257b05e902dd314a59c9297078e60908eabd
hechen316 <hechen316@163.com>
2014-09-11 21:21:06 +0800

    Create SecondClass.java

---
+++ helloworld/src/helloworld/SecondClass.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("the second class \n it is created in internet!!!");
+	}
+
+}
